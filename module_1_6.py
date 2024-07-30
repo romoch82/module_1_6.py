c21088765de5f20e35a094fe8918d778996639a7
romoch82 <romoch37@gmail.com>
2024-07-30 22:09:35 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,14 @@
+my_dict = {"roman": 1982, "dima": 2009}
+print(my_dict)
+print(my_dict["roman"])
+my_dict["lena"] = 1986
+my_dict["max"] = 1981
+del my_dict["dima"]
+print(my_dict)
+
+my_set_ = {5,9,2,7,5,2,6,4,9,}
+print(my_set_)
+my_set_.add("false")
+my_set_.add("True")
+my_set_.remove(7)
+print(my_set_)
