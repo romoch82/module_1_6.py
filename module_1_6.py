8c612291b73aad93c148123b7f8c9ee211b21a5f
romoch82 <romoch37@gmail.com>
2024-11-01 21:34:45 +0300

    Update module_1_6.py

--- module_1_6.py
+++ module_1_6.py
@@ -3,7 +3,7 @@ print(my_dict)
 print(my_dict["roman"])
 print(my_dict.get("sasha")
 my_dict.update({"lena": 1986 "max": 1981})
-del my_dict["dima"]
+my_dict.pop("dima")
 print(my_dict)
 
 my_set_ = {5,9,2,7,5,2,6,4,9,}
